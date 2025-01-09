0c86eb38c008d3b9cbece0ee745d3190d2ed6c72
Zora Che <zche@umd.edu>
2025-01-09 20:05:56 +0000

    utils added

---
+++ utils/jailbreak_utils.py
@@ -0,0 +1,135 @@
+from datasets import load_dataset
+import torch
+import os
+from strong_reject.evaluate import evaluate_dataset
+EVALUATOR = "strongreject_rubric"
+openai_api_key = os.getenv("OPENAI_API_KEY")
+if openai_api_key is None:
+    raise ValueError("OpenAI API key not found. Please set the OPENAI_API_KEY environment variable.")
+
+
+def format_text(prompt, response=None):
+    # uses the llama3-8b-it template
+    # https://pytorch.org/torchtune/0.2/tutorials/chat.html
+    system_prompt = "You are a helpful, respectful, and honest assistant."
+    formatted_text =  (
+        "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+        f"{system_prompt}"
+        "<|eot_id|>\n"
+
+        "<|start_header_id|>user<|end_header_id|>\n\n"
+        f"{prompt}"
+        "<|eot_id|>\n"
+
+        "<|start_header_id|>assistant<|end_header_id|>\n\n"
+    )
+    if response is not None:
+        formatted_text += f"{response}<|eot_id|>"
+    return formatted_text
+
+def load_jb_dataset(eval_size=1000, seed=42, include_test_responses=False, batch_size=None, rejected_pair=False, suffix=""):
+    # Load the dataset from Hugging Face
+    dataset = load_dataset("LLM-LAT/harmful-dataset")
+
+    # Set random seed for reproducibility
+    dataset = dataset.shuffle(seed=seed)
+
+    # Split the dataset
+    train_dataset = dataset["train"].select(range(64))
+    if eval_size is None:
+        eval_dataset = dataset["train"].select(range(64, len(dataset["train"])))
+    else:
+        eval_dataset = dataset["train"].select(range(64, eval_size + 64))
+
+    def add_suffix(example, sfx=""):
+        example["prompt"] = example["prompt"] + " "+ sfx
+        return example
+
+    eval_dataset_with_suffix = eval_dataset.map(lambda x: add_suffix(x, sfx=suffix))
+
+    def format_example(example, response=True):
+        rejected_response = example['rejected']
+        chosen_response = example['chosen']
+        if response:
+            return {
+                "formatted_rejected": format_text(example['prompt'], rejected_response),
+                "formatted_chosen": format_text(example['prompt'], chosen_response),
+                "formatted_prompt": format_text(example['prompt'])
+            }
+        else:
+            return {"formatted_prompt": format_text(example['prompt'])}
+
+    # Create train and eval datasets
+    train_dset = train_dataset.map(lambda x: format_example(x, response=include_test_responses))
+    eval_dset = eval_dataset_with_suffix.map(lambda x: format_example(x))
+
+    # Option to return batched dataset
+    if batch_size is not None:
+        if rejected_pair:
+            train_dset = [train_dset['formatted_rejected'][i:i + batch_size] for i in range(0, len(train_dset), batch_size)]
+        else:
+            train_dset = [train_dset['formatted_prompt'][i:i + batch_size] for i in range(0, len(train_dset), batch_size)]
+
+    return train_dset, eval_dset
+
+
+
+def load_ultrachat(seed=42,train_size=64,batch_size=None):
+
+    # Load the dataset from Hugging Face
+    dataset = load_dataset("HuggingFaceH4/ultrachat_200k")
+
+    # Set random seed for reproducibility
+    dataset = dataset.shuffle(seed=seed)
+    dataset = dataset["train_sft"].select(range(train_size))
+    
+
+    # function to format examples into templated prompt+response format
+    def format_example(example):
+        assert example['messages'][0]['role'] == 'user'
+        assert example['messages'][1]['role'] == 'assistant'
+        return {"formatted_prompt_response": format_text(example['messages'][0]['content'],
+                                                         example['messages'][1]['content'])}
+
+    # Create formatted dataset
+    dataset = dataset.map(lambda x: format_example(x))
+    if batch_size is not None:
+        dataset = [dataset['formatted_prompt_response'][i:i + batch_size] for i in range(0, len(dataset), batch_size)]
+    
+
+    return dataset
+
+def evaluate_jailbreak_robustness(model, tokenizer, dataset, batch_size=16):
+    responses = []
+
+    # dataset = dataset.select(range(num_examples))
+    for i in range(0, len(dataset), batch_size):
+        batch_prompts = dataset['formatted_prompt'][i:i + batch_size]
+        # Tokenize prompts in the batch
+        inputs = tokenizer(batch_prompts, return_tensors="pt", padding=True, truncation=True).to("cuda")
+        # Generate responses
+        with torch.no_grad():
+            outputs = model.generate(**inputs, max_length=300)  # Adjust max_length as needed
+        # Decode responses and add them to the list
+        batch_responses = tokenizer.batch_decode(outputs, skip_special_tokens=False)
+        responses.extend(batch_responses)
+    
+    responses = [extract_assistant_response(r) for r in responses]
+
+    dataset = dataset.add_column("response", responses)
+    dataset = dataset.add_column("forbidden_prompt", dataset['prompt'])
+
+    eval_dataset = evaluate_dataset(dataset, [EVALUATOR])
+    return eval_dataset  # np.mean(eval_dataset['score']) will give the average compliance: 0 is robust, 1 is compliant
+
+def extract_assistant_response(text):
+        try:
+            # Split by the assistant marker and take the last part
+            parts = text.split("<|start_header_id|>assistant<|end_header_id|>\n\n")
+            if len(parts) < 2:
+                return ""
+            # Split by EOT marker and take the content
+            response = parts[-1].split("<|eot_id|>")[0].strip()
+            return response
+        except Exception:
+            return ""  # Fallback in case of parsing errors
---
+++ utils/unlearn_utils.py
@@ -0,0 +1,218 @@
+import json
+import torch
+import os
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch.nn.functional as F
+import random
+from typing import List, Optional
+import tempfile
+from peft import PeftModel
+from torch.utils.data import Dataset, DataLoader
+from datasets import load_dataset
+
+random.seed(0)
+
+
+
+
+class CustomDataset(Dataset):
+    def __init__(self, forget_corpora, data_dir, min_len=50, max_len=2000):
+        self.data = self.get_corpus_data(forget_corpora[0], data_dir, min_len, max_len)
+
+    def get_corpus_data(self, name, data_dir, min_len, max_len):
+        data = []
+        if name == "wikitext":
+            raw_data = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+            for x in raw_data:
+                if min_len < len(x['text']) <= max_len:
+                    data.append(str(x['text']))
+        else:
+            with open(f"{data_dir}/data/{name}.jsonl", "r") as f:
+                for line in f:
+                    if "bio-forget-corpus" in name or "bio-remove-dataset" in name:
+                        raw_text = json.loads(line)['text']
+                    else:
+                        raw_text = line
+                    if min_len < len(raw_text) <= max_len:
+                        data.append(str(raw_text))
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]   #, self.labels[idx]
+
+
+
+
+
+def truncate_and_pad_two_batches(batch1, batch2, pad_token_id=0):
+    # Find the minimum batch size between the two batches
+    min_batch_size = min(len(batch1['input_ids']), len(batch2['input_ids']))
+    
+    # Truncate batches to the minimum batch size
+    batch1 = {k: v[:min_batch_size] for k, v in batch1.items()}
+    batch2 = {k: v[:min_batch_size] for k, v in batch2.items()}
+    
+    # Find the maximum sequence length across both batches
+    max_len = max(
+        max(seq.size(0) for seq in batch1['input_ids']),
+        max(seq.size(0) for seq in batch2['input_ids'])
+    )
+    
+    def pad_batch(batch, max_len):
+        padded_input_ids = []
+        padded_attention_mask = []
+        
+        for ids, mask in zip(batch['input_ids'], batch['attention_mask']):
+            pad_len = max_len - ids.size(0)
+            
+            padded_ids = F.pad(ids, (0, pad_len), value=pad_token_id)
+            padded_input_ids.append(padded_ids)
+            
+            padded_mask = F.pad(mask, (0, pad_len), value=0)
+            padded_attention_mask.append(padded_mask)
+        
+        return {
+            'input_ids': torch.stack(padded_input_ids),
+            'attention_mask': torch.stack(padded_attention_mask)
+        }
+    
+    # Pad both batches
+    padded_batch1 = pad_batch(batch1, max_len)
+    padded_batch2 = pad_batch(batch2, max_len)
+    
+    return padded_batch1, padded_batch2
+
+
+
+def forward_with_cache(model, inputs, module, no_grad=True):
+    # define a tensor with the size of our cached activations
+    cache = []
+    def hook(module, input, output):
+        if isinstance(output, tuple):
+            cache.append(output[0])
+        else:
+            cache.append(output)
+        return None 
+    
+    hook_handle = module.register_forward_hook(hook)
+    
+    if no_grad:
+        with torch.no_grad():
+            _ = model(**inputs)
+    else:
+        _ = model(**inputs)
+        
+    hook_handle.remove()
+
+    return cache[0]
+    
+
+
+def get_params(model, layer_ids, param_ids):
+    params = []
+    for layer_id in layer_ids:
+        for i, p in enumerate(model.model.layers[layer_id].parameters()):
+            if i in param_ids:
+                params.append(p)
+    return params
+
+
+def load_model(model_name_or_path):
+    
+    torch_dtype = "auto" if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else torch.float16
+
+    try:
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name_or_path,
+            torch_dtype=torch_dtype,
+            trust_remote_code=True,
+            device_map="auto"
+        )
+    except Exception as e:
+        base_model= AutoModelForCausalLM.from_pretrained(
+             "meta-llama/Meta-Llama-3-8B-Instruct",
+            torch_dtype=torch_dtype,
+            trust_remote_code=True,
+            device_map="auto"
+        )
+        lora_model = PeftModel.from_pretrained(base_model, model_name_or_path)
+        model = lora_model.merge_and_unload()
+        del base_model
+        del lora_model
+
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path, 
+            trust_remote_code=True, 
+            use_fast=False
+
+        )
+    except Exception as e:
+        tokenizer = AutoTokenizer.from_pretrained(
+            "meta-llama/Meta-Llama-3-8B-Instruct", 
+            trust_remote_code=True, 
+            use_fast=False
+
+        )
+
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.padding_side = "left"
+    tokenizer.mask_token_id = tokenizer.eos_token_id
+    tokenizer.sep_token_id = tokenizer.eos_token_id
+    tokenizer.cls_token_id = tokenizer.eos_token_id
+
+    return model, tokenizer
+
+
+def get_data(forget_corpora, retain_corpora, min_len=50, max_len=2000, batch_size=4):
+    def get_dataset(name):
+        data = []
+        if name == "wikitext":
+            from datasets import load_dataset
+            raw_data = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+            for x in raw_data:
+                if len(x['text']) > min_len:
+                    data.append(str(x['text']))
+        elif name =="bio-retain-corpus":
+            from datasets import load_dataset
+            raw_data = load_dataset("cais/wmdp-corpora", "bio-retain-corpus", split="train")
+            for x in raw_data:
+                if len(x['text']) > min_len:
+                    data.append(str(x['text']))
+        elif name == "imdb":
+            from datasets import load_dataset
+            raw_data = load_dataset("stanfordnlp/imdb", split="train")
+            for x in raw_data:
+                if len(x['text']) > min_len:
+                    data.append(str(x['text']))
+        else:
+            for line in open(f"{data_dir}/data/{name}.jsonl", "r"):
+                if "bio-forget-corpus" in name:
+                    raw_text = json.loads(line)['text']
+                elif "bio-remove-dataset" in name:
+                    raw_text = json.loads(line)['text']
+                else:
+                    raw_text = line
+                if len(raw_text) > min_len:
+                    data.append(str(raw_text))
+        data = [data[i:i + batch_size] for i in range(0, len(data), batch_size)]
+        return data
+
+    return (
+        [get_dataset(c) for c in forget_corpora],
+        [get_dataset(c) for c in retain_corpora]
+    )
+
+
+
+def get_loss(output, labels):
+    shifted_labels = labels[..., 1:].contiguous()
+    output = output[..., :-1, :].contiguous()
+
+    loss_function = nn.CrossEntropyLoss(ignore_index=-100)
+    loss = loss_function(output.view(-1, output.size(-1)), shifted_labels.view(-1))
+
+    return loss
